b1ce50edcf672d755292d8c10b19354c06a55c7c
Chrissy Semens <chrissy.semens@bedegaming.com>
2020-05-29 09:04:59 +0100

    [PATCH] - Retrieve port from env for Heroku

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import { HelloGimmie } from "./hello-gimmie";
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 var path = require('path');
 
 export const client = new Phelia(process.env.SLACK_TOKEN)
